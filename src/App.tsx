c00512709391fd2d863f10e1d23beacee131bba9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-03 09:01:39 +0900

    axiosでデータ取得準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
 
 function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
 }
