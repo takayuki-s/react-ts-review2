9b8cdedaac40b1e833076e325934d39ee844bc78
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-04 08:50:05 +0900

    propsに型定義

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userId={todo.userId}/>
+        <Todo title={todo.title} userId={todo.userId} completed={todo.completed}/>
       ))}
     </div>
   );
