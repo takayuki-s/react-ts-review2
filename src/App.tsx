5a9ef4515c2e764aa185b7eb292cd7295eb9608c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-04 05:56:04 +0900

    axiosでAPI取得（型指定なしver）

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Todo } from './Todo';
 
 function App() {
+  const [ todos, setTodos ] = useState<any>([]);
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId}/>
+      ))}
     </div>
   );
 }
