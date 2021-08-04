495d9045b71dd5e13ea4cc73706c4b52c50a03a1
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-04 09:04:52 +0900

    型定義の効率化
    Omitは不必要な型のみ選択

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,7 +1,7 @@
 import { TodoType } from "./types/todo"
 
 
-export const Todo = (props: Pick<TodoType, "userId" | "title" | "completed" >) => {
+export const Todo = (props: Omit<TodoType, "id" >) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return (
